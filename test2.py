94b544013ba849250c8fcac76217e09af169db93
Michael PUlis <97983542+wordle-malti@users.noreply.github.com>
2022-04-10 19:23:46 +0200

    Made a lot of changes... Most imp of all, almost ready with making multiple predictions.

--- test2.py
+++ test2.py
@@ -1,3 +1,20 @@
-from FootballEnv import FootballEnv
+import cv2
+from create_dataset import CreateDataset
 
-env = FootballEnv()
+
+ds = CreateDataset()
+ds.loadTrackingContentFromDir('three-sixty/*.json')
+# datasetMaker.file_limit = 5
+ds.loadFilesFromDir('events/*.json', filterGamesWithoutTrackingData=True)
+observations, actions, rewards, event_ids, terminals = ds.createImageDataset()
+
+# showing
+
+print(observations.shape)
+print('avg', observations[0].mean()) 
+
+cv2.imshow('image', observations[0][0])
+cv2.imshow('image1', observations[0][1])
+cv2.imshow('image12', observations[0][2])
+                                                 
+cv2.waitKey(0)
